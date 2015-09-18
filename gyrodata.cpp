34d18073252bd22a86a12dddf16265d97ba16b3f
catalips <artthirt@gmail.com>
2015-09-18 21:34:55 +0300

    correct

--- gyrodata.cpp
+++ gyrodata.cpp
@@ -45,17 +45,17 @@ QString GyroData::fileName() const
 
 inline double get_min(const QVector3D& v, double min)
 {
-	double res = qMin(min, v.x());
-	res = qMin(min, v.y());
-	res = qMin(min, v.z());
+	double res = qMin<double>(min, v.x());
+	res = qMin<double>(min, v.y());
+	res = qMin<double>(min, v.z());
 	return res;
 }
 
 inline double get_max(const QVector3D& v, double max)
 {
-	double res = qMax(max, v.x());
-	res = qMax(max, v.y());
-	res = qMax(max, v.z());
+	double res = qMax<double>(max, v.x());
+	res = qMax<double>(max, v.y());
+	res = qMax<double>(max, v.z());
 	return res;
 }
 
@@ -166,53 +166,6 @@ void search_min_pt(const QVector< QVector3D >& data, QVector3D& min, QVector3D& 
 	 }
 }
 
-QVector3D get_center(const QVector< QVector3D >& data, double &mean_radius)
-{
-	if(!data.size())
-		return QVector3D();
-
-	QVector3D min, max;
-	search_min_pt(data, min, max);
-
-//	QVector3D v = max - min;
-
-//	bool loop = true;
-
-//	while(loop){
-//		double x_beg = min.x(), dx = v.x() / 10;
-
-//		while(x_beg < max.x()){
-
-//			double y_beg = min.y(), dy = v.y() / 10;
-
-//			while( y_beg < max.y()){
-
-//				double z_beg = min.z(), dz = v.z() / 10;
-
-//				while(z_beg < max.z()){
-
-
-
-//					z_beg += dz;
-//				}
-
-//				y_beg += dy;
-//			}
-
-//			x_beg += dx;
-//		}
-//	}
-	//center *= 1.0/data.size();
-
-//	mean_radius = 0;
-//	foreach (QVector3D it, data) {
-//		mean_radius += QVector3D(it - center).length();
-//	}
-//	mean_radius /= data.size();
-
-	return QVector3D();
-}
-
 bool search_outliers(const QVector3D& center, double mean_radius, double threshold,
 					 QVector< QVector3D >& data, QVector< QVector3D >& out, QVector< int >& errors)
 {
@@ -277,71 +230,7 @@ QVector3D get_pt_on_line(const QVector3D& p0, const QVector3D& n, double t)
 
 inline double sign(double v1)
 {
-	return v1 > 0? 1.0 : -1.0;
-}
-
-bool get_cp_sphere(const QVector3D& cp_circle, const QVector3D& n,
-				   const QVector3D &pA, const QVector3D &pB, const QVector3D &pC,
-				   QVector3D& po, double& R)
-{
-	double d = 1, d_min = 0.00001;
-	double t1 = -10, t2 = 10, tmp;
-	QVector3D p0, p1;
-	double R01, R02, R03, R11, R12, R13;
-
-	int index = 0, max_index = 100;
-
-/// for correct
-//	do{
-//		p0 = get_pt_on_line(cp_circle, n, t1);
-//		p1 = get_pt_on_line(cp_circle, n, t2);
-
-//		R01 = QVector3D(pA - p0).length();
-//		R02 = QVector3D(pB - p0).length();
-//		R03 = QVector3D(pC - p0).length();
-
-//		R11 = QVector3D(pA - p1).length();
-//		R12 = QVector3D(pB - p1).length();
-//		R13 = QVector3D(pC - p1).length();
-
-//		double w1 = qAbs(R01 - R02) + qAbs(R01 - R03) + qAbs(R02 - R03);
-//		double w2 = qAbs(R11 - R12) + qAbs(R11 - R13) + qAbs(R12 - R13);
-
-//		if(qFuzzyIsNull(w1)){
-//			R = R01;
-//			po = p0;
-//			return true;
-//		}
-//		if(qFuzzyIsNull(w2)){
-//			R = R11;
-//			po = p1;
-//			return true;
-//		}
-
-//		//double dt = qAbs(t2 - t1);
-//		d = w1 + w2;
-//		if(d < d_min)
-//			break;
-
-//		if(w1 > w2){
-//			tmp = t1;
-//			t1 = t2;
-//			t2 = t2 + (tmp - t2);
-//		}else{
-//			tmp = t2;
-//			t2 = t1;
-//			t1 = t1 + (tmp - t1) * 0.5;
-//		}
-
-//		index++;
-//	}while(d > d_min && index < max_index);
-
-	qDebug() << "loop for searh:" << index << ". state: " << (index < max_index);
-
-	R = R01;
-	po = p0;
-
-	return (index < max_index);
+	return v1 >= 0? 1.0 : -1.0;
 }
 
 void GyroData::draw()
@@ -355,12 +244,12 @@ void GyroData::draw()
 	}
 	glEnd();
 
-//	glColor3f(1, 0, 0);
-//	glBegin(GL_POINTS);
-//	foreach (QVector3D it, m_gyro_data) {
-//		glVertex3d(it.x(), it.y(), it.z());
-//	}
-//	glEnd();
+	glColor3f(1, 0, 0);
+	glBegin(GL_POINTS);
+	foreach (QVector3D it, m_gyro_data) {
+		glVertex3d(it.x(), it.y(), it.z());
+	}
+	glEnd();
 
 	glPointSize(7);
 	glColor3f(1, 1, 0);
@@ -390,60 +279,6 @@ void GyroData::draw()
 
 	glLineWidth(1);
 
-	do{
-		QVector3D pA, pB, pC, pD, p1, p2, dp1, dp2, n, m, cpABC;
-		pA = m_accel_data[50];
-		pB = m_accel_data[220];
-		pC = m_accel_data[430];
-		pD = m_accel_data[333];
-
-		p1 = (pA + pB) * 0.5;
-		p2 = (pB + pC) * 0.5;
-
-		dp1 = pB - pA;
-		dp2 = pC - pB;
-
-		cpABC = QVector3D::crossProduct(dp1, dp2).normalized();
-		n = QVector3D::crossProduct(dp1, cpABC).normalized();
-		m = QVector3D::crossProduct(dp2, cpABC).normalized();
-
-		QVector3D p0 = p1 - p2;
-
-		double v1 = n.x() * m.y() - n.y() * m.x();
-		double v2 = p0.y() * m.x() - p0.x() * m.y();
-
-		if(qFuzzyIsNull(v1))
-			continue;
-
-		double k1 = v2 / v1;
-
-		QVector3D cp1p = n * k1 + p1;
-
-		n = p1 - cp1p;
-		m = p2 - cp1p;
-
-		n = QVector3D::crossProduct(m, n).normalized();
-
-		QVector3D po;
-		double R;
-		bool res = get_cp_sphere(cp1p, n, pA, pB, pD, po, R);
-		if(!res)
-			continue;
-		qDebug() << " radius: "<< R;
-
-		draw_line(pA, pB);
-		draw_line(pB, pC);
-		draw_line(pC, pA);
-		draw_line(pC, pD);
-		draw_line(pD, pA);
-
-		draw_line(pA, po, Qt::red);
-		draw_line(pB, po, Qt::red);
-		draw_line(pC, po, Qt::red);
-
-		draw_line(p1, cp1p, Qt::green);
-		draw_line(p2, cp1p, Qt::green);
-	}while(0);
 //	draw_line(p1, cp1m);
 //	draw_line(p2, cp1m);
 //	draw_line(p3, cp1m);
